ff8040a7c7a79ccc750c31dfdacc499c07baccdc
Ernest Voon <ernestvooncs@outlook.com>
2020-09-30 17:30:03 +0800

    Included VM guest installation

--- packer_templates/windows/scripts/vm-guest-tools.ps1
+++ packer_templates/windows/scripts/vm-guest-tools.ps1
@@ -1,4 +1,6 @@
 # Install the Oracle certificate first
-certutil -addstore -f "TrustedPublisher" E:\cert\oracle-vbox.cer
+certutil -addstore -f "TrustedPublisher" E:\cert\vbox-sha256.cer
+certutil -addstore -f "TrustedPublisher" E:\cert\vbox-sha1.cer
+
 
 Start-Process -FilePath "E:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
